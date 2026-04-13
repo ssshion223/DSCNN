b504a08725062c215ef3fcbd52dce627d2c7d627
Ianchow <Ianchow@mail.cn>
2026-04-13 18:35:33 +0800

    project

--- verilog/matrix_conv2d_stream/conv_coeff_store.v
+++ verilog/matrix_conv2d_stream/conv_coeff_store.v
@@ -1,47 +1,32 @@
-/*
-输入要读取通道，输出相应通道的卷积核
-
-*/
-
-
 module conv_coeff_store #(
-    parameter integer COEFF_W      = 8,
-    parameter integer K            = 3,
-    parameter integer OUT_CH       = 1,
-    parameter         INIT_FILE  = "",
-
-    parameter integer COEFF_CH_W   = ((OUT_CH <= 1) ? 1 : $clog2(OUT_CH))
+    parameter integer COEFF_W       = 8,
+    parameter integer K_H           = 3,
+    parameter integer K_W           = 3,
+    parameter integer OUT_CH        = 1,
+    parameter integer COEFF_GRP_NUM = 1,
+    parameter         INIT_FILE     = "",
+    parameter integer COEFF_GRP_W   = ((COEFF_GRP_NUM <= 1) ? 1 : $clog2(COEFF_GRP_NUM))
 )(
     input  wire                                  clk,
-    input  wire [COEFF_CH_W-1:0]                 coeff_rd_ch,
-    output reg  signed [K*K*COEFF_W-1:0]         coeff_bus
+    input  wire [COEFF_GRP_W-1:0]                coeff_rd_grp,
+    output reg  signed [OUT_CH*K_H*K_W*COEFF_W-1:0] coeff_bus
 );
-    localparam integer WIN_SIZE      = K * K;
-    localparam integer COEFF_TOTAL   = OUT_CH * WIN_SIZE;
-    localparam integer COEFF_TOTAL_W = (COEFF_TOTAL <= 1) ? 1 : $clog2(COEFF_TOTAL);
+    localparam integer WIN_SIZE      = K_H * K_W;
+    localparam integer GROUP_SIZE    = OUT_CH * WIN_SIZE;
+    localparam integer BUS_WIDTH     = GROUP_SIZE * COEFF_W;
 
-    (*rom_style = "distributed" *)reg signed [COEFF_W-1:0] coeff_mem [0:COEFF_TOTAL-1];
+    // 关键修改：定义一个“浅而宽”的存储器
+    // 深度 = 组数 (COEFF_GRP_NUM)，宽度 = 一次卷积所需的所有权重总位宽 (BUS_WIDTH)
+    reg [BUS_WIDTH-1:0] coeff_mem [0:COEFF_GRP_NUM-1];
 
-    wire [31:0] coeff_rd_addr_full;
-    wire [COEFF_TOTAL_W-1:0] coeff_rd_addr;
-
-    integer i;
-
-    assign coeff_rd_addr_full = coeff_rd_ch * WIN_SIZE;
-    assign coeff_rd_addr      = coeff_rd_addr_full[COEFF_TOTAL_W-1:0];
-
-  
     initial begin
-		$readmemh(INIT_FILE, coeff_mem);
+        // 注意：这里的初始化文件内容也需要适配宽数据格式！
+        $readmemh(INIT_FILE, coeff_mem);
     end
 
-    
+    // 读逻辑：一个时钟周期只进行 1 次读操作，完美契合单端口 BRAM！
     always @(posedge clk) begin
-        for (i = 0; i < WIN_SIZE; i = i + 1) begin
-            coeff_bus[(i+1)*COEFF_W-1 -: COEFF_W] <= coeff_mem[coeff_rd_addr + i];
-        end
+        coeff_bus <= coeff_mem[coeff_rd_grp]; 
     end
 
- 
- 
 endmodule
